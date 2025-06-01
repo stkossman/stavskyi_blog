09c484c29660ce2d337120d556d3b61801ea2c60
stkossman <an.stawski@outlook.com>
2025-06-01 23:28:42 +0000

    admin panel for categories

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,15 @@ Route::group([
 ], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
 });
+
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index', 'edit', 'store', 'update', 'create'];
+    Route::resource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
